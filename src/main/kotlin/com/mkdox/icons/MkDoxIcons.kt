6c096a3254510ae789e305a65432b554f405370c
Peter Forret <peter@forret.com>
2025-09-18 11:49:45 +0200

    ADD: MkDoxIcons.kt mkdoxActive.svg,

---
+++ src/main/kotlin/com/mkdox/icons/MkDoxIcons.kt
@@ -0,0 +1,8 @@
+package com.mkdox.icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object MkDoxIcons {
+    val active: Icon = IconLoader.getIcon("/icons/mkdoxActive.svg", MkDoxIcons::class.java)
+}
